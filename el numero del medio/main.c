b293c8776939678c3799142a9d98ba99549bfe62
Daniel <morono.daniel99@gmail.com>
2018-08-27 12:11:23 -0300

    Mas cambios en el archivo

--- el numero del medio/main.c
+++ el numero del medio/main.c
@@ -3,18 +3,62 @@
 
 int main()
 {
-    int primerNumero;
-    int segundoNumero;
-    int tercerNumero;
+    int numeroUno;
+    int numeroDos;
+    int numeroTres;
+    int numeroDelMedio;
+    int maximo;
+    int minimo;
 
-    printf("Ingrese un numero\n");
-    scanf("%d", primerNumero);
+    printf("Ingrese un numero entero\n");
+    scanf("%d",numeroUno);
+    maximo = numeroUno;
+    minimo = numeroUno;
 
-    printf("Ingrese otro numero\n");
-    scanf("%d", segundoNumero);
+    printf("Ingrese otro numero entero\n");
+    scanf("%d",& numeroDos);
+    if (numeroDos < minimo)
+    {
+        minimo = numeroDos;
+    }
+    else {
+        if (numeroDos > maximo)
+        {
+            maximo = numeroDos;
+        }
+    }
 
-    printf("Ingrese otro numero\n");
-    scanf("%d", tercerNumero);
+    printf("Ingrese otro numero entero\n");
+    scanf("%d", numeroTres);
+     if (numeroTres< minimo)
+    {
+        minimo = numeroTres;
+    }
+    else {
+        if (numeroTres > maximo)
+        {
+            maximo = numeroTres;
+        }
+    }
 
+    if (numeroUno != maximo && numeroUno != minimo)
+    {
+
+    }
+    else {
+            if (numeroDos != maximo && numeroDos != minimo)
+            {
+                numeroDos = numeroDelMedio;
+            }
+            else {
+                if (numeroTres != maximo && numeroTres != minimo)
+                {
+                    numeroTres = numeroDelMedio;
+                }
+            }
+        }
+
+
+    printf("El numero del medio es : %d",numeroDelMedio);
     return 0;
 }
