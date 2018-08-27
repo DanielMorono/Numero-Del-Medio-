5186da0ce87b68f962fdb51ce75194602e5276cd
Daniel <morono.daniel99@gmail.com>
2018-08-27 11:29:08 -0300

    Cambios en el archivo

---
+++ el numero del medio/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int primerNumero;
+    int segundoNumero;
+    int tercerNumero;
+
+    printf("Ingrese un numero\n");
+    scanf("%d", primerNumero);
+
+    printf("Ingrese otro numero\n");
+    scanf("%d", segundoNumero);
+
+    printf("Ingrese otro numero\n");
+    scanf("%d", tercerNumero);
+
+    return 0;
+}
